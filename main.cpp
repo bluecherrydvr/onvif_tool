2744d16faea4b0e3119f2f0b9ab66c496228dee8
Anton <anton@corp.bluecherry.net>
2021-05-24 04:56:18 +0400

    update copyright years

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2020 Bluecherry, LLC
- * Copyrigh (c) 2020 Anton Sviridenko
+ * Copyright (c) 2021 Bluecherry, LLC
+ * Copyrigh (c) 2021 Anton Sviridenko
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
